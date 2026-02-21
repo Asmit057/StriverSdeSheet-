005567e1926fca673533d1f7bad98ab26af856d0
Asmit057 <124249676+Asmit057@users.noreply.github.com>
2026-02-21 10:12:41 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (32.92%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -22,4 +22,21 @@ public:
         mexcount = max(mexcount,count);
         return mexcount;
     }
+    // or
+
+//     for(int i = 0; i < nums.size(); i++)
+// {
+//     if(nums[i] == 0)
+//     {
+//         mexcount = max(mexcount, count);  // update before reset
+//         count = 0;
+//     }
+//     else
+//     {
+//         count++;
+//     }
+// }
+
+// // Final check (important if array doesn’t end with 0)
+// mexcount = max(mexcount, count);
 };
