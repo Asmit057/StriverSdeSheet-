6b165591117a749f4d6d9bc5a4ad99ac08101baf
Asmit057 <124249676+Asmit057@users.noreply.github.com>
2026-04-23 10:22:47 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (7.87%) - LeetSync

--- 01Array/169-majority-element/majority-element.cpp
+++ 01Array/169-majority-element/majority-element.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       // u can use hashmap but space is O(n) amd time(nlogn), so use moore's voting algo
-       // arr[4] = [2,2,4,4] count=2, then=0, so no majority element
-       // arr[5] = [2,2,4,4,4] count=2,then=0,then =1, so majority element =5
-       int count=0;
-       int ele = nums[0];
-      for(int i=0; i<nums.size(); i++ )
-      {
-        if(nums[i]== ele)
+        int count=0;
+        int temp = nums[0];
+        for(int i=0; i<nums.size(); i++)
         {
-            count++;
+            
+         if(nums[i]!=temp)
+         {
+            count--;
+         }
+         else{
+         count++;
+         }
+         if(count==0)
+         {
+            temp = nums[i];
+            count=1;
+         }
         }
-       else{
-        count--;
-       }
-       if(count==0)
-       {
-        ele = nums[i];
-        count++;
-       }
-      }
-     return ele;
+        return temp;
     }
 };
