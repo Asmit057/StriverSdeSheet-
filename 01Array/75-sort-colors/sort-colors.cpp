cfb569b9ad4bd25b642d0f90e8236a41999d6317
Asmit057 <124249676+Asmit057@users.noreply.github.com>
2026-05-11 20:56:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (77.53%) - LeetSync

--- 01Array/75-sort-colors/sort-colors.cpp
+++ 01Array/75-sort-colors/sort-colors.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low=0; 
-        int mid=0;
+        int low =0;
+        int mid =0;
         int high = nums.size()-1;
+        // we are moving only mid , we are keeping low on side
+        // and high one side
         while(mid<=high)
         {
             if(nums[mid]==0)
@@ -11,15 +13,15 @@ public:
                 swap(nums[low],nums[mid]);
                 low++;
                 mid++;
-            }
+                // increase the window of low
+        }
             else if(nums[mid]==1)
             {
-                swap(nums[low],nums[mid]);
-              //  low++;
                 mid++;
             }
             else{
                 swap(nums[mid],nums[high]);
+               // mid++;
                 high--;
             }
         }
