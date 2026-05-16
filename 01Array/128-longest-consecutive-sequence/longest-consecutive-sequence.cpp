bf7fafcc04d3f0f5345e93eb643ea35dd8f37e1e
Asmit057 <124249676+Asmit057@users.noreply.github.com>
2026-05-16 20:27:29 +0530

    Time: 79 ms (69.20%) | Memory: 89 MB (49.58%) - LeetSync

--- 01Array/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 01Array/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> st;
+        // first store all the elements in the map
+        // bcz searching in linear time
+         unordered_set<int> st;
         
         for(int i=0; i<nums.size(); i++)
         {
@@ -12,9 +14,9 @@ public:
         // iterate only or move count only if the x is the start of the sequence
         for(int x : st )
         {
-            int count=1;
+            int count=0;
            if(st.find(x-1)==st.end()){
-            while(st.find(x+1)!=st.end())
+            while(st.find(x)!=st.end())
             {
                 count++;
                 x++;
@@ -25,5 +27,5 @@ public:
             
 
         return mCount;
-        }
+    }
 };
