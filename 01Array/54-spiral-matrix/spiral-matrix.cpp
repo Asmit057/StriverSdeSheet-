1f8039a053600c3204a742b22966d98c1318201d
Asmit057 <124249676+Asmit057@users.noreply.github.com>
2026-05-17 18:06:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (13.89%) - LeetSync

--- 01Array/54-spiral-matrix/spiral-matrix.cpp
+++ 01Array/54-spiral-matrix/spiral-matrix.cpp
@@ -2,42 +2,44 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int top =0;
-        int left=0;
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int bottom = n-1;
-        int right = m-1;
-        vector<int> result;
+        int bottom = matrix.size()-1;
+        int left = 0;
+        int right = matrix[0].size()-1;
+        vector<int>result;
         while(top<=bottom && left<=right)
         {
-            // from left to right
+            //1. traverse from left to right then top++
             for(int i=left; i<=right; i++)
             {
-         result.push_back(matrix[top][i]);
+                result.push_back(matrix[top][i]);
             }
-          top++; // this row is traversed
-            // now top to bottom
-            for(int i=top;i<=bottom ; i++)
+            top++; // 1st row traversed
+            //2. traverse last column from top to bottom 
+            // and now u traverse the right end so right --
+            for(int i=top; i<=bottom; i++)
             {
                 result.push_back(matrix[i][right]);
             }
             right--;
-            // right to left
+            //3. now traverse from right to left
+            // and bottom --
             if(top<=bottom){
-            for(int i=right;i>=left ; i--)
+            for(int i=right; i>=left; i--)
             {
                 result.push_back(matrix[bottom][i]);
             }
             bottom--;
             }
-            // now from bottom to top
+            //4.now traverse from bottom to up
+            // ans left ++ bcz that col traversed
             if(left<=right){
-            for(int i=bottom;i>=top ; i--)
+            for(int i=bottom ; i>=top; i--)
             {
                 result.push_back(matrix[i][left]);
             }
-              left++;
+            left++;
             }
+
         }
         return result;
     }
