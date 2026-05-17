f8e70a8f15c87e63da0f99940097b04dbf48a169
Asmit057 <124249676+Asmit057@users.noreply.github.com>
2026-05-17 17:02:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (7.33%) - LeetSync

--- 01Array/48-rotate-image/rotate-image.cpp
+++ 01Array/48-rotate-image/rotate-image.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        // swap matarix[i][j] with matrix[j][i]
-        // then reverse the row
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int a=0;
-int b=0;
-for(int i=a; i<n; i++)
-{
-    for(int j= b; j<m; j++)
-    {
-        swap(matrix[i][j],matrix[j][i]);
-    }
-    a++, b++;
-}
+    // first change rows to column and column to rows(transpose)
+    // so swap diagonallly and reverse
+    // for swapping diagonally j start from k and after iteration 
+    // for j is over then increment k 
+    int k=0;
+    int row = matrix.size();
+    int col = matrix[0].size();
 
-for(int i=0; i<n; i++)
-{
-    reverse(matrix[i].begin(),matrix[i].end());
-}
+    for(int i=0; i<row ; i++)
+    {
+        for(int j=k; j<col; j++)
+        {
+         swap(matrix[i][j],matrix[j][i]);
+        }
+        k++;
+    }
+
+    // now after taking the transpose reverse the array
+    for(int i=0; i<row; i++)
+    {
+        reverse(matrix[i].begin(),matrix[i].end());
+    }
     }
 };
