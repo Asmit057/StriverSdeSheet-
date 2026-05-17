e6af70a40075d11040960773286f7f5ad6b61065
Asmit057 <124249676+Asmit057@users.noreply.github.com>
2026-05-17 12:21:23 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (49.07%) - LeetSync

--- 01Array/73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 01Array/73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-       int extraCol0=1;
-       int n= matrix.size();
-       int m= matrix[0].size();
-       for(int i=0; i<n; i++)
-       {
-        for(int j=0; j<m; j++)
+    void setZeroes(vector<vector<int>>& nums) {
+        // first set the row and col to 0
+        // intially if any element in 0th col is 0 then we
+        //  mark col0 as 0
+        int col0 = 1;
+        int row = nums.size();
+        int col = nums[0].size();
+        for(int i=0; i<row; i++)
         {
-            if(matrix[i][j]==0)
+            for(int j=0; j<col; j++)
             {
-             matrix[i][0] =0;
-             if(j!=0){
-             matrix[0][j] =0;
-             }
-         else{
-                extraCol0=0;
+                if(nums[i][j]==0)
+                {
+                    if(j!=0){
+                    nums[i][0] =0;
+                    nums[0][j] =0;
+                    }
+                    else{
+                        col0 = 0;
+                    }
+                }
             }
+        }
+        // now making the matrix =0;
+        for(int i=row-1; i>=0; i--)
+        {
+            for(int j=col-1; j>=0; j--)
+            {
+                if((nums[0][j]==0 || nums[i][0]==0) && (j!=0))
+                {
+                    nums[i][j] = 0;
+                }
+                else if(j==0){
+                if(col0 == 0)
+                {
+                    nums[i][j] =0;
+                }
+                }
             }
+        }
 
-        }
-       } 
-     for(int i = n-1; i >= 0; i--)
-{
-    for(int j = m-1; j >= 0; j--)
-    {
-        if(j != 0)
-        {
-            if(matrix[0][j] == 0 || matrix[i][0] == 0)
-            {
-                matrix[i][j] = 0;
-            }
-        }
-        else if(extraCol0 == 0)
-        {
-            matrix[i][0] = 0;
-        }
-    }
-}
     }
 };
