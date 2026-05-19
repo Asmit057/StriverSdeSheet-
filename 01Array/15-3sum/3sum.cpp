fe80248089bca3ea87c75b935fe7c49795c15156
Asmit057 <124249676+Asmit057@users.noreply.github.com>
2026-05-19 09:16:29 +0530

    Time: 76 ms (13.74%) | Memory: 34.9 MB (12.43%) - LeetSync

--- 01Array/15-3sum/3sum.cpp
+++ 01Array/15-3sum/3sum.cpp
@@ -1,52 +1,49 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // key idea fix one element i and then move j and k acc. to i
-        // tc. O(n2) bcz checking for every element.
-          sort(nums.begin(), nums.end());
-
-    vector<vector<int>> result;
-    int n = nums.size();
-
-    for(int i = 0; i < n; i++)
-    {
-        // skip duplicate i
-        if(i > 0 && nums[i] == nums[i-1])
-            continue;
-
-        int j = i + 1;
-        int k = n - 1;
-
-        while(j < k)
+        int n= nums.size();
+        vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
+        for(int i=0; i<n; i++)
         {
-            int sum = nums[i] + nums[j] + nums[k];
-
-            if(sum == 0)
+            if(i!=0 && nums[i]==nums[i-1])
             {
-                result.push_back({nums[i], nums[j], nums[k]});
-
+                continue;
+            }
+           
+            int j= i+1;
+            int k= n-1;
+            while(j<k)
+            {
+             
+                int sum= nums[i]+nums[j]+nums[k];
+                if(sum==0)
+                {
+                vector<int> v;
+                v.push_back(nums[i]);
+                v.push_back(nums[j]);
+                v.push_back(nums[k]);
                 j++;
                 k--;
-
-                // skip duplicates
-                while(j < k && nums[j] == nums[j-1])
+                result.push_back(v);
+                while(j<k && nums[j]==nums[j-1])
+                {
                     j++;
-
-                while(j < k && nums[k] == nums[k+1])
+                }
+                while(j<k && nums[k]==nums[k+1])
+                {
                     k--;
-            }
-            else if(sum > 0)
-            {
-                k--;
-            }
-            else
-            {
-                j++;
+                }
+                }
+                else if(sum>0)
+                {
+                    k--;
+                }
+                else{
+                    j++;
+                }
             }
         }
-    }
-
-    return result;
-        
+        return result;
     }
 };
