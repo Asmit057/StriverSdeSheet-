e9eb59568a5d5710ccac0cada183322c2e10d301
Asmit057 <124249676+Asmit057@users.noreply.github.com>
2026-05-19 07:58:18 +0530

    Time: 3 ms (51.96%) | Memory: 23.8 MB (44.81%) - LeetSync

--- 01Array/229-majority-element-ii/majority-element-ii.cpp
+++ 01Array/229-majority-element-ii/majority-element-ii.cpp
@@ -1,59 +1,64 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int cand1 = -1;
-        int cand2 =-1;
-        int count1=0;
-        int count2 =0;
-        int n = nums.size();
-        vector <int> v;
+        // as we know that there are only two elements which can be
+        // appear more than n/3 times bcz (n/3+n/3+n/3) =n
+        // so find two candidate elements by using moore voting algo
+      int cand1= nums[0];
+      int cand2 ;
+      int n = nums.size();
+      int count=0;
+      int count2=0;
+      for(int i=0; i<n; i++)
+      {
+        if(count<=0 &&(nums[i]!=cand2))
+        {
+            cand1= nums[i];
+            count=0;
+        }
+        if(count2<=0 &&(nums[i]!=cand1))
+        {
+            cand2= nums[i];
+            count2=0;
+        }
+        if(nums[i]==cand1)
+        {
+            count++;
+        }
+       else if(nums[i]==cand2)
+       {
+        count2++;
+       }
+       else{
+        count--;
+        count2--;
+       }
 
-        for(int i=0; i<n;i++)
+      }
+      cout<<"count2 "<<count2<<endl;
+      cout<<"cand1 "<<cand1<<" cand2 "<<cand2;
+      int c1=0;
+      int c2=0;
+      vector<int> result;
+      for(int i=0; i<n; i++)
+      {
+        if(nums[i]==cand1)
         {
-            if(nums[i]==cand1)
-            {
-                count1++;
-            }
-            else if(nums[i]== cand2)
-            {
-                count2++;
-            }
-            else if(count1==0)
-            {
-                cand1 = nums[i];
-                count1=1;
-            }
-            else if(count2==0)
-            {
-                cand2 = nums[i];
-                count2 = 1;
-            }
-            else{
-                count1--;
-                count2--;
-            }
+            c1++;
         }
-        int count3=0;
-        int count4=0;
-        for(int i=0; i<n; i++)
+        if(nums[i]==cand2)
         {
-            if(nums[i]== cand1)
-            {
-                count3++;
-            }
-            else if(nums[i]== cand2)
-            {
-                count4++;
-            }
-        } 
-        if(count3>(n/3))
-        {
-            v.push_back(cand1);
+            c2++;
         }
-        if(count4>(n/3))
-        {
-            v.push_back(cand2);
-        }
-        return v;
+      }
+      if(c1>n/3)
+      {
+        result.push_back(cand1);
+      }
+      if(c2>n/3)
+      {
+        result.push_back(cand2);
+      }
+      return result;
     }
 };
