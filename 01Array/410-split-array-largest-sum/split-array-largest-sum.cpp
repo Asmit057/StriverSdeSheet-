fdbdfd49e216bd4cb73e6e2ef6561125e847a728
Asmit057 <124249676+Asmit057@users.noreply.github.com>
2026-07-10 23:25:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (42.52%) - LeetSync

--- 01Array/410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 01Array/410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,60 +1,55 @@
 class Solution {
 public:
-//Binary search relies on this property:
-
-//If mid is feasible, try a smaller mid (move left).
-// if mid is not feasible, try a larger mid (move right).
-//  mid is the maximum allowed sum of any subarray.
-
-// "Can I split the array so that every subarray has sum ≤ mid, using at most k subarrays?"
-
-// If the answer is yes, then mid is feasible.
-
-    bool feasible(vector<int>& nums,int mid , int k)
+   bool feasible(vector<int>& nums, int k , int mid)
+   {
+    int count =1 ;
+    int sum =0;
+    int n= nums.size();
+    for(int i=0; i<n; i++)
     {
-        int sum =0;
-        int countsub = 1;
-        int n = nums.size();
-        for(int i =0; i<n; i++)
+        if(nums[i]>mid)
         {
-            if(sum + nums[i]<= mid)
-            {
-                sum += nums[i];
-            }
-            else{
-                countsub++;
-                sum = nums[i];
-            }
+            return false;
         }
-    if(countsub<=k)
-     {
-        return true;
-    }
-       
-        return false;
-    }
-    int splitArray(vector<int>& nums, int k) {
-        int n = nums.size();
-        int low  = INT_MIN;
-        int high =0;
-        int result = -1;
-        for(int i=0; i<n; i++)
+        if(sum + nums[i]<= mid)
         {
-      high += nums[i];
-      low  = max(low, nums[i]);
-        }
-        while(low<=high)
-        {
-        int mid = (low+high)/2;
-        bool x = feasible(nums, mid,k);
-        if(x)
-        {
-            result = mid;
-            high = mid-1;
+            sum += nums[i];
         }
         else{
-            low = mid+1;
+            sum = nums[i];
+            count++;
         }
+    }
+    if(count <= k)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+   }
+    int splitArray(vector<int>& nums, int k) {
+        int low = INT_MIN;
+        int high  =0;
+        int n = nums.size();
+        for(int i=0 ;i<n; i++)
+        {
+            low = max(low, nums[i]);
+            high += nums[i];
+        }
+        int result = -1;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            bool x =  feasible(nums,k,mid);
+            if(x)
+            {
+                result = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
         }
         return result;
     }
