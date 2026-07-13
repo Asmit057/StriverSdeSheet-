c784ed0b881cbe79972db9bbd6254e94773da959
Asmit057 <124249676+Asmit057@users.noreply.github.com>
2026-07-13 23:10:50 +0530

    Time: 3 ms (0.46%) | Memory: 8.2 MB (97.69%) - LeetSync

--- 01Array/1212-sequential-digits/sequential-digits.cpp
+++ 01Array/1212-sequential-digits/sequential-digits.cpp
@@ -1,72 +1,113 @@
 class Solution {
 public:
-  bool check(int firstdigit)
+  int createseq(int countdigit)
+{
+  long long  int firstdigit = 0;
+  long long  int x = 0;
+  long long  int c= 1;
+    while(x<countdigit)
     {
-        int temp = firstdigit;
-        int c = 0;
-
-        while (temp > 0)
-        {
-            temp /= 10;
-            c++;
-        }
-
-        if (c == 1)
-            return false;
-
-        int x1 = firstdigit % 10;
-        firstdigit /= 10;
-
-        while (firstdigit > 0)
-        {
-            int x2 = firstdigit % 10;
-
-            if (x1 - x2 != 1)
-                return false;
-
-            x1 = x2;
-            firstdigit /= 10;
-        }
-
-        return true;
+        firstdigit  = firstdigit*10 + c;
+        c++;
+        x++;
     }
-
-    vector<int> sequentialDigits(int low, int high)
+        return firstdigit;
+}
+   int inc1(int countdigit)
+   {
+   long long int addone =0;
+     long long    int x = 0;
+       long long  int c= 1;
+    while(x<countdigit)
     {
-        vector<int> result;
-
-        // Count digits of low
-        int countdigit = 0;
-        int temp = low;
-
-        while (temp > 0)
+    addone  = addone*10 + c;
+    x++;
+    }
+    return addone;
+   }
+   bool check(int firstdigit)
+   {
+   long long int temp = firstdigit;
+   long long int c=1;
+    while(temp>0)
+    {
+        temp = temp/10;
+        c++;
+    }
+    if(c==1)
+    {
+        return false;
+    }
+    int x1 = firstdigit%10;
+    firstdigit = firstdigit/10;
+    while(firstdigit>0)
+    {
+        int x2 = firstdigit%10;
+        if(x1-x2!=1)
         {
-            countdigit++;
-            temp /= 10;
+      return false;
         }
-
-        while (countdigit <= 9)
+        x1 =x2;
+        firstdigit = firstdigit/10;
+    }
+    return true;
+   }
+    vector<int> sequentialDigits(int low, int high) {
+    vector<int> result;
+    int countdigit = 0;
+ 
+ int temp = low;
+ while(temp>0)
+   {
+    temp = temp/10;
+    countdigit++;
+   }
+  long long int firstdigit = createseq(countdigit);
+   
+ if(firstdigit==0)
+ {
+    return result;
+ }
+int lastdigit = firstdigit%10;
+  cout<<"firstdigit "<<firstdigit<<endl;
+  cout<<"countdigit "<<countdigit<<endl;
+    while(firstdigit<=high)
+    {
+        // repersenting the first number in that digit ex-> 4didigts
+        
+        if(lastdigit==9)
         {
-            // Starting digit can be at most (10-countdigit)
-            for (int start = 1; start <= 10 - countdigit; start++)
-            {
-                int num = 0;
-
-                for (int d = start; d < start + countdigit; d++)
-                {
-                    num = num * 10 + d;
-                }
-
-                if (num > high)
-                    return result;
-
-                if (num >= low && check(num))
-                    result.push_back(num);
+            if(check(firstdigit) && firstdigit>=low){
+            result.push_back(firstdigit);
             }
-
-            countdigit++;
+            countdigit  += 1;
+            firstdigit = 0;
+             int x = 0;
+             int c= 1;
+             while(x<countdigit)
+             {
+                firstdigit  = firstdigit*10 + c;
+                c++;
+                x++;
+             }
+             cout<<"in lsstdigit loop "<<firstdigit<<endl;
+        }
+        if(firstdigit>=low && firstdigit<= high)
+        {
+            cout<<"first digit in if "<<firstdigit<<endl;
+            bool y = check(firstdigit);
+            cout<<" y is "<<y<<endl;
+            if(y){
+            result.push_back(firstdigit);
+            }
+           
+             
         }
 
-        return result;
+        //low = firstdigit;
+        firstdigit = firstdigit + inc1(countdigit);
+       lastdigit = firstdigit%10;
+    }
+    return result;
     }
 };
