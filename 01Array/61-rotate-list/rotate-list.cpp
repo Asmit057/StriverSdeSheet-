fe229cd47005bf3e69001911bc7b64f91c678ba8
Asmit057 <124249676+Asmit057@users.noreply.github.com>
2026-07-28 08:35:30 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (33.31%) - LeetSync

--- 01Array/61-rotate-list/rotate-list.cpp
+++ 01Array/61-rotate-list/rotate-list.cpp
@@ -11,34 +11,38 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
+         if(head==NULL || k==0 )
+        {
+            return head;
+        }
+        int count=0;
+        ListNode * temp = head;
+        while(temp)
+        {
+        count++;
+        temp = temp->next;
+        }
         
- if(head == NULL || k == 0)
-    return head;
-int count = 0;
-ListNode *temp = head;
-while(temp)
-{
-    count++;
-    temp = temp->next;
-}
-k %= count;
-if(k == 0)
-    return head;
-int c1 = count - k;
-ListNode *temp2 = head;
-while(c1 > 1)
-{
-    temp2 = temp2->next;
-    c1--;
-}
-ListNode *head1 = temp2->next;
-temp2->next = NULL;
-ListNode *temp3 = head1;
-while(temp3->next)
-{
-    temp3 = temp3->next;
-}
-temp3->next = head;
-return head1;
+        k = k%count;
+        if(k==0)
+        {
+            return head;
+        }
+        int c1 = count-k;
+        ListNode *temp2 = head;
+        while(c1>1 && temp2!=NULL)
+        {
+            temp2 = temp2->next;
+            c1--;
+        }
+        ListNode *head1 = temp2->next;
+        temp2->next= NULL;
+        ListNode * temp3 = head1;
+        while(temp3 && temp3->next)
+        {
+            temp3 = temp3->next;
+        }
+        temp3->next = head;
+        return head1;
     }
 };
